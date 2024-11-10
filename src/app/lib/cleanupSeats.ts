84f86e79019d79546531984734c6b4503ed3b582
GastonMora42 <gaston-mora@hotmail.com>
2024-11-10 20:08:50 -0300

    efefef

--- src/app/lib/cleanupSeats.ts
+++ src/app/lib/cleanupSeats.ts
@@ -6,7 +6,7 @@ export async function releaseExpiredSeats() {
   try {
     const now = new Date();
 
-    // Encontrar asientos expiradosssssss
+    // Encontrar asientos expirados
     const expiredSeats = await Seat.find({
       status: 'RESERVED',
       reservationExpires: { $lt: now }
